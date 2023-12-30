85d9e5a531d42c39f3bece1caf1bbcceaaafaf87
pilgrim_tabby <pilgrimtabby@gmail.com>
2023-12-30 10:36:06 +0900

    Update __init__.py

--- src/manage_savestates/__init__.py
+++ src/manage_savestates/__init__.py
@@ -4,5 +4,6 @@ explanation of this package's purpose and operation.
 import os
 import sys
 
+
 # Allow importing between modules without issues.
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
